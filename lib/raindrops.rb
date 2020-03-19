36c73ec3f6f6128f81010a48574ad916e7bb715c
Sasho1989 <caki.jova@gmail.com>
2020-03-19 13:09:24 +0100

    Adding new tests for Raindrops

--- lib/raindrops.rb
+++ lib/raindrops.rb
@@ -1,6 +1,17 @@
 require "raindrops/version"
 
 module Raindrops
-  class Error < StandardError; end
-  # Your code goes here...
+  class Convert_to_str
+    FACTORS = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
+
+    def initialize(num)
+      @num = num
+    end
+
+    def convert
+      converting = ""
+      FACTORS.map { |factor, output| converting += output if @num % factor == 0 }
+      converting = converting.empty? ? @num.to_s : converting
+    end
+  end
 end
--- spec/raindrops_spec.rb
+++ spec/raindrops_spec.rb
@@ -3,7 +3,23 @@ RSpec.describe Raindrops do
     expect(Raindrops::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'outputs Pling if number has 3 as a factor' do
+    rnds = Raindrops::Convert_to_str.new(3)
+    expect(rnds.convert).to eq('Pling')
+  end
+
+  it 'outputs Plang if number has 5 as a factor' do
+    rnds = Raindrops::Convert_to_str.new(5)
+    expect(rnds.convert).to eq('Plang')
+  end
+
+  it 'outputs Plong if number has 7 as a factor' do
+    rnds = Raindrops::Convert_to_str.new(7)
+    expect(rnds.convert).to eq('Plong')
+  end
+
+  it "returns back the num if it doesn't include 3, 5, 7 as factor" do
+    rnds = Raindrops::Convert_to_str.new(17)
+    expect(rnds.convert).to eq('17')
   end
 end
